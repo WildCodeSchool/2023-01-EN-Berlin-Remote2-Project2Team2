a232eb5c16e86262129f843a441a00fb97fb6a0b
Yevhenii Airapetian <yevhenii-airapetian_student2023@wilder.school>
2023-03-08 19:17:13 +0100

    add filterresults, change dropdown

---
+++ backend/filterresults.js
@@ -0,0 +1,11 @@
+function FilterResults(){
+    if (option.value === "Beginner") {
+      return <div>Beginner</div>;
+    }
+    else if (option.value === "Intermediate") {
+      return <div>Intermediate</div>;
+    }
+    else if (option.value === "Expert") {
+      return <div>Expert</div>;
+    }
+  };
