a7ddc98eb0fea51b0d3d915f73ddd8660e733032
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-03-09 11:44:09 +0100

    Structure created and video loop added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
-import DropdownDifficulty from "./components/dropdowndifficulty";
+import "./App.scss";
+import ropes from "./assets/ropes.mp4";
 
 function App() {
-  return <DropdownDifficulty />;
+  return (
+    <>
+      <div className="App">
+        <video src={ropes} autoPlay loop muted />
+        <div className="Container">
+          <div className="Animation">
+            <h1>Animation</h1>
+          </div>
+          <div className="SearchAndSelect">
+            <div className="Searchbar">
+              <h2>Searchbar</h2>
+            </div>
+            <div className="Select">
+              <h2>Select 1</h2>
+            </div>
+            <div className="Select">
+              <h2>Select 2</h2>
+            </div>
+            <div className="Select">
+              <h2>Select 3</h2>
+            </div>
+          </div>
+        </div>
+        <div className="Results">
+          <h1>Results</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
