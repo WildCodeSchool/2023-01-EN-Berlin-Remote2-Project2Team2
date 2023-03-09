58e0d7ff883ee44566374f9c023c38baa59a7f55
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-03-09 13:23:58 +0100

    icons added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.scss";
 import ropes from "./assets/ropes.mp4";
+import levelsvg from "./assets/barbellcolor-03.svg";
+import equipmentsvg from "./assets/equipment-01.svg";
+import bodypartysvg from "./assets/body-01.svg";
 
 function App() {
   return (
@@ -16,13 +19,16 @@ function App() {
               <h2>Searchbar</h2>
             </div>
             <div className="Select">
-              <h2>Select 1</h2>
+              <h2>Select Your Level</h2>
+              <img src={levelsvg} alt="" />
             </div>
             <div className="Select">
-              <h2>Select 2</h2>
+              <h2>Select Your Equipment</h2>
+              <img src={equipmentsvg} alt="" />
             </div>
             <div className="Select">
-              <h2>Select 3</h2>
+              <h2>Select Your Body Part</h2>
+              <img src={bodypartysvg} alt="" />
             </div>
           </div>
         </div>
